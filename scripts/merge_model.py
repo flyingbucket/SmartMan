ce5c82a2ae4f99cfedb990ded973bf4c023f08a9
flyingbucket <2311067@mail.nankai.edu.cn>
2026-05-02 16:42:13 +0800

    chore: update merge script

--- scripts/merge_model.py
+++ scripts/merge_model.py
@@ -1,14 +1,20 @@
 from peft import PeftModel
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from argparse import ArgumentParser
 
-baase_model_name = "Qwen2.5-Coder-1.5B"
-adapter_name = "qwenInstruct_short0501_0107"
-merged_path = f"./dist/{baase_model_name}_{adapter_name}"
+p = ArgumentParser()
+p.add_argument("--base_model_name", type=str)
+p.add_argument("--adapter_name", type=str)
+args = p.parse_args()
+base_model_name = args.base_model_name
+adapter_name = args.adapter_name
+
+merged_path = f"./dist/{base_model_name}_{adapter_name}"
 
 # 显式指定使用 CPU 加载
 base_model = AutoModelForCausalLM.from_pretrained(
-    f"./base_models/{baase_model_name}",
+    f"./base_models/{base_model_name}",
     torch_dtype=torch.float32,
     device_map="cpu",
 )
@@ -20,11 +26,11 @@ model = PeftModel.from_pretrained(
 )
 
 # 合并并保存
-merged_model = model.merge_and_unload()
+merged_model = model.merge_and_unload()  # type:ignore
 merged_model.save_pretrained(merged_path, safe_serialization=True)
 
 # 处理 Tokenizer
-tokenizer = AutoTokenizer.from_pretrained(f"./base_models/{baase_model_name}")
+tokenizer = AutoTokenizer.from_pretrained(f"./base_models/{base_model_name}")
 tokenizer.save_pretrained(merged_path)
 
-print(f"成功合并并保存至: {merged_path}")
+print(f"Merged and saved to: {merged_path}")
